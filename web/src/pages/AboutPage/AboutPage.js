b25bc6106a55e84c3d8749d798942328d43ea4ca
YOUR NAME <YOUR@EMAIL.com>
2020-04-28 23:26:41 -0800

    Added a BlogLayout

---
+++ web/src/pages/AboutPage/AboutPage.js
@@ -0,0 +1,14 @@
+import { Link, routes } from '@redwoodjs/router'
+import BlogLayout from 'src/layouts/BlogLayout'
+
+const AboutPage = () => {
+  return (
+    <BlogLayout>
+      <p>
+        This is a blog site using RedwoodJS that I am working on.  To demonstrate GIFDat.com
+      </p>
+    </BlogLayout>
+  )
+}
+
+export default AboutPage
--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -1,10 +1,7 @@
+import BlogLayout from 'src/layouts/BlogLayout'
+
 const HomePage = () => {
-  return (
-    <div>
-      <h1>HomePage</h1>
-      <p>Find me in ./web/src/pages/HomePage/HomePage.js</p>
-    </div>
-  )
+  return <BlogLayout>Home</BlogLayout>
 }
 
 export default HomePage
