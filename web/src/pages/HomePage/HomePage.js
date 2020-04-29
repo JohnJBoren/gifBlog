45cf52d7ef608b350f200c8d0f58cd0010ada6d3
YOUR NAME <YOUR@EMAIL.com>
2020-04-29 09:11:48 -0800

    Added blog scaffolding

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -1,7 +1,14 @@
+// web/src/pages/HomePage/HomePage.js
+
 import BlogLayout from 'src/layouts/BlogLayout'
+import BlogPostsCell from 'src/components/BlogPostsCell'
 
 const HomePage = () => {
-  return <BlogLayout>Home</BlogLayout>
+  return (
+    <BlogLayout>
+      <BlogPostsCell />
+    </BlogLayout>
+  )
 }
 
 export default HomePage
